6e3e5f7df332b54e02ff7168fd2f7d0020dc8c53
dev-debasis <debasiskhamari7@gmail.com>
2024-08-20 00:56:50 +0530

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose"
+import {DB_NAME} from "../constants.js"
+
+// While connecting to DB always write with try-catch as error can occur in case of DB.
+// write async function as DB is in another continent so it takes time to fetch
+let connectDB = async () => {
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB Connected !!! `);
+        console.log(`\n Checking the connectionInstance: ${connectionInstance}`);
+        console.log(`\n DB HOST: ${connectionInstance.connection.host}`);
+        
+    }catch(error){
+        console.error("MONGODB Connection Failed: ", error)
+        process.exit(1)
+    }
+}
+
+export default connectDB;
